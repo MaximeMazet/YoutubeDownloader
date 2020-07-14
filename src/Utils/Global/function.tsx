456d6c9a041234de163ad6f1a7c245f7c1543972
Maxime Mazet <contact@maxime-mazet.fr>
2020-07-14 18:59:44 +0200

    Add new function for the back send file

--- src/Utils/Global/function.tsx
+++ src/Utils/Global/function.tsx
@@ -2,7 +2,8 @@ import './variable'
 import { backend_url } from './variable'
 
 export {
-    fetchBack
+    fetchBack,
+    fetchBackFile
 }
 
 /**
@@ -24,5 +25,23 @@ async function fetchBack(url: string, method: string, headers: string[][], body:
     } else {
         console.log(`This request got an error ${response.status}`)
         return await response.json()
+    }
+}
+/**
+ * 
+ * @param url The route to the back without the domain
+ * @param method the method of request (GET, POST ....)
+ * @param headers The header of the request
+ * @param body The body
+ */
+async function fetchBackFile(url: string, method: string, headers: string[][], body: Object){
+    let response = await fetch(`${backend_url}${url}`, {
+        headers,
+        method,
+        body: JSON.stringify(body)
+    })
+
+    if (response.ok) {
+        return await response.blob()
     }
 }
