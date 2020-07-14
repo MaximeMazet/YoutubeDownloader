dce4cd6ae605d54e743f3fe07debdd240f33e4be
Maxime Mazet <contact@maxime-mazet.fr>
2020-07-14 17:06:27 +0200

    Add loader

---
+++ src/Components/Modal/Loading.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import './Loading.scss'
+
+export const Loading = () => {
+    const {t} = useTranslation()
+    return(
+        <section className="loading-modal">
+            <img src="/assets/images/loading.gif" />
+            <p>{t('_WORKING_LOADING')}</p>
+        </section>
+    )
+}
